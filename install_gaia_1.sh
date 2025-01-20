6a32afc09ff81412111ca843338f9d40530979a0
predator-bites <142865899+predator-bites@users.noreply.github.com>
2025-01-21 01:23:32 +0200

    Update install_gaia_1.sh

--- install_gaia_1.sh
+++ install_gaia_1.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# Цветовые коды
+NEON_RED='\033[38;5;196m'
+NEON_BLUE='\033[38;5;45m'
+RESET='\033[0m'
+
+# Логотип
+logo() {
+    echo -e "
+${NEON_RED}  ____   ${NEON_BLUE}____  
+${NEON_RED} |  _ \\  ${NEON_BLUE}|  _ \\ 
+${NEON_RED} | | | | ${NEON_BLUE}| |_) |
+${NEON_RED} | |_| | ${NEON_BLUE}|  __/ 
+${NEON_RED} |____/  ${NEON_BLUE}|_|    
+${NEON_RESET}
+"
+}
+
+# Вызов логотипа
+logo
+echo -e "${NEON_BLUE}https://t.me/DropPredator${RESET}"
+# Основная логика
+echo -e "${NEON_RED}Welcome to DP Script!${RESET}"
+
 # Первая часть установки Gaianet Node
 set -e
 
@@ -15,6 +38,3 @@ sudo apt update -y && sudo apt-get update -y
 
 # Устанавливаем Gaianet
 curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash -s -- --base $NODE_DIR
-
-# Завершаем первую часть установки
-echo "Первая часть установки завершена. Запустите следующий скрипт после выполнения команды source /root/.bashrc"
