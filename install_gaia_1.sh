fa0afa59b8421f352aa2f5a788671b912cccc451
predator-bites <142865899+predator-bites@users.noreply.github.com>
2025-01-21 02:23:55 +0200

    Update install_gaia_1.sh

--- install_gaia_1.sh
+++ install_gaia_1.sh
@@ -13,7 +13,7 @@ ${NEON_RED} |  _ \\  ${NEON_BLUE}|  _ \\
 ${NEON_RED} | | | | ${NEON_BLUE}| |_) |
 ${NEON_RED} | |_| | ${NEON_BLUE}|  __/ 
 ${NEON_RED} |____/  ${NEON_BLUE}|_|    
-${NEON_RESET}
+${RESET}
 "
 }
 
@@ -32,16 +32,18 @@ echo -e "${NEON_BLUE}Введите номер ноды (например, 2): $
 # Чтение ввода
 read NODE_NUMBER
 
+# Условие для создания директории для первой ноды
 if [ "$NODE_NUMBER" -eq 1 ]; then
     NODE_DIR="/root/gaianet"
 else
     NODE_DIR="/root/gaianet-$NODE_NUMBER"
 fi
 
-mkdir  $NODE_DIR
+# Создаем директорию для ноды
+mkdir -p "$NODE_DIR"  # Используем -p для безопасного создания директорий
 
 # Устанавливаем обновления системы
 sudo apt update -y && sudo apt-get update -y
 
 # Устанавливаем Gaianet
-curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash -s -- --base $NODE_DIR
+curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash -s -- --base "$NODE_DIR"
